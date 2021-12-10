bbdcc373935170fe6767643f2cbdefbfc3a2ca09
jerome4995 <c.r.jerome4995@gmail.com>
2021-12-10 12:54:16 +0530

    filter console and changes

--- src/components/tailor/FormTest.js
+++ src/components/tailor/FormTest.js
@@ -1,43 +1,77 @@
+import { objectExpression } from "@babel/types";
 import React, { useState, useEffect } from "react";
+import { orderRef } from "../../firebase/Firebase";
 
 const FormTest = () => {
-  // const Values = {
-  //   Name: "",
-  //   Email: "",
-  // };
-  // const [initialState, setInitialState] = useState(Values);
-  // const { Name, Email } = initialState;
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-  // const handleInputChange = () => {};
-  // const handleSubmit = () => {
-  //   console.log("Button Clicked");
-  // };
+  const [firstNameErr, setFirstNameErr] = useState({});
+  const [lastNameErr, setLastNameErr] = useState({});
+
+  const handleInputChange = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const inValid = formValidation();
+  };
+
+  const formValidation = () => {
+    const firstNameErr = {};
+    const lastNameErr = {};
+    let isValid = true;
+
+    if (firstName.trim().length < 5) {
+      firstNameErr.firstNameShort = "Firstname Is Too Short";
+      isValid = false;
+    }
+    if (firstName.trim().length < 10) {
+      firstNameErr.firstNameBig = "Firstname Is Too Big";
+      isValid = false;
+    }
+    if (!lastName.includes("123")) {
+      lastNameErr.lastname123 = "LastName Must Have 123";
+      isValid = false;
+    }
+    setFirstNameErr(firstNameErr);
+    setLastNameErr(lastNameErr);
+    return isValid;
+  };
 
   return (
     <div>
-      {/* <form>
+      <form>
         <label>
-          Enter your name:
+          Enter your Firstname:
           <input
             type="text"
-            value={Name}
-            name="Name"
-            onchange={handleInputChange}
+            value={firstName}
+            name="firstName"
+            onchange={(e) => {
+              setFirstName(e.target.value);
+            }}
           />
         </label>
+        <br />
+        {Object.keys(firstNameErr).map((key) => {
+          return <div style={{ color: "red" }}>{firstNameErr[key]}</div>;
+        })}
+        <label>Enter your Lastname </label>
+        <input
+          type="text"
+          value={lastName}
+          name="lastName"
+          onchange={(e) => {
+            setLastName(e.target.value);
+          }}
+        />
+        <br />
+        {Object.keys(lastNameErr).map((key) => {
+          return <div style={{ color: "red" }}>{lastNameErr[key]}</div>;
+        })}
         <label>
-          Enter your email:
-          <input
-            type="email"
-            value={Email}
-            name="Email"
-            onchange={handleInputChange}
-          />
+          <input type="button" value="submit" onClick={handleSubmit} />
         </label>
-        <label>
-          <input type="button" value="Send" onClick={handleSubmit} />
-        </label>
-      </form> */}
+      </form>
     </div>
   );
 };
